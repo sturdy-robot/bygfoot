d508fe12b4cf53d20fca0897bd96ddf7a76a3d3a
Tom Stellard <tom@stellard.net>
2022-12-11 16:53:54 -0800

    ci: Add mingw builds
    ref #1

--- scripts/package.sh
+++ scripts/package.sh
@@ -27,7 +27,23 @@ mkdir -p $installdir
 # Don't use the -B option here, because some older versions of
 # cmake don't support it.
 pushd $builddir
-cmake -G Ninja  -S $srcdir -DCMAKE_INSTALL_PREFIX=$installdir_abs -DCMAKE_C_FLAGS=-DVERS=\"\\\"$version\\\"\"
+if [ "$BYGFOOT_MINGW" -eq 1 ]; then
+    git clone https://github.com/json-c/json-c
+    pushd json-c
+    git checkout json-c-0.16-20220414
+    mkdir -p install
+    json_c_installdir=`pwd`/install
+    mkdir -p build
+    pushd build
+    cmake -G Ninja -S .. -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_FIND_ROOT_PATH=/usr/x86_64-w64-mingw32/sys-root/ -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=$json_c_installdir
+    ninja install
+    popd
+    popd
+
+    cmake -G Ninja  -S $srcdir -DCMAKE_INSTALL_PREFIX=$installdir_abs -DCMAKE_C_FLAGS=-DVERS=\"\\\"$version\\\"\" -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_FIND_ROOT_PATH=/usr/x86_64-w64-mingw32/sys-root/ -DJSON-C_DIR=$json_c_installdir/lib/cmake/json-c/ -DZLIB_LIBRARY=/usr/x86_64-w64-mingw32/sys-root/mingw/lib/libz.dll.a -DZLIB_INCLUDE_DIR=/usr/x86_64-w64-mingw32/sys-root/mingw/include/ -DPKG_CONFIG_EXECUTABLE=/usr/bin/mingw64-pkg-config -DJSON-C_LIBRARIES=json-c::json-c-static
+else
+    cmake -G Ninja  -S $srcdir -DCMAKE_INSTALL_PREFIX=$installdir_abs -DCMAKE_C_FLAGS=-DVERS=\"\\\"$version\\\"\"
+fi
 popd
 
 ninja -v -C $builddir install
@@ -36,12 +52,14 @@ ninja -v -C $builddir install
 # Fixup install dir so that bygfoot can find the support
 # files.
 
-mv $installdir/bin/bygfoot $installdir/
+mv $installdir/bin/bygfoot* $installdir/
 rmdir $installdir/bin
 
 mv $installdir/share/bygfoot/support_files $installdir
 
-mv $installdir/share/locale $installdir
+if [ -d $installdir/share/locale ]; then
+    mv $installdir/share/locale $installdir
+fi
 rmdir $installdir/share/bygfoot
 rmdir $installdir/share
 
