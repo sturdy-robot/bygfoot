21f7ca80c4f6efc10b6fa6197fb30454f15e2f13
Tom Stellard <tom@stellard.net>
2023-12-24 22:09:11 -0800

    Remove unused macro

--- src/league.h
+++ src/league.h
@@ -33,7 +33,6 @@
 
 #define league_cup_get_fixtures(clid) (clid < ID_CUP_START) ? (league_from_clid(clid)->fixtures) : (cup_from_clid(clid)->fixtures)
 #define league_cup_get_name_string(clid) (clid < ID_CUP_START) ? league_from_clid(clid)->name : cup_from_clid(clid)->name
-#define league_cup_get_yellow_red(clid) (clid < ID_CUP_START) ? (league_from_clid(clid)->yellow_red) : (cup_from_clid(clid)->yellow_red)
 
 #define league_table_cumul(league) (&g_array_index((league)->tables, Table, 0))
 #define league_table(league) (&g_array_index((league)->tables, Table, league->tables->len - 1))
