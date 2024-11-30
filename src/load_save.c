0a1c31a40f2426f82fbae0a9e0df2b1f25c9378f
Tom Stellard <tom@stellard.net>
2024-11-29 19:43:52 -0800

    Don't call language_set() when loading a game
    This gets called when bygfoot starts up, and calling it again
    causes duplicate entries to be added to the list of news articles.

--- src/load_save.c
+++ src/load_save.c
@@ -326,7 +326,6 @@ load_save_load_game(Bygfoot *bygfoot, const gchar* filename, gboolean create_mai
     file_load_opt_file(buf->str, &options, FALSE);
     g_string_printf(buf, "%s%s%s___settings", dirname, G_DIR_SEPARATOR_S, prefix);
     file_load_opt_file(buf->str, &settings, FALSE);
-    language_set(bygfoot, language_get_code_index(opt_str("string_opt_language_code")) + 1);
 
     if(debug > 60)
         g_print("load_save_load leagues \n");
